98099667d8692666131b9398c01330346d4eeac7
Карпова Ксения <ksenia.karpova.4@mail.ru>
2024-08-07 17:09:29 +0300

    о

--- mail.php
+++ mail.php
@@ -1,52 +1,40 @@
-
-<?php 
+<?php
+// Включаем логирование ошибок
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email'];
+// Получаем данные из формы
+$name = $_POST['user_name'] ?? '';
+$phone = $_POST['user_phone'] ?? '';
+$email = $_POST['user_email'] ?? '';
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+// Настройки SMTP
+$mail->isSMTP();
+$mail->Host = 'smtp.yandex.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'zheskarkarpov@yandex.ru';
+$mail->Password = 'qljqdbatmwfgfqup';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-// $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-// $mail->SMTPAuth = true;                               // Enable SMTP authentication
-// $mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-// $mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
-// $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-// $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+// Настройки отправителя и получателя
+$mail->setFrom('zheskarkarpov@yandex.ru', 'Заявка с сайта');
+$mail->addAddress('ksenia.karpova.4@mail.ru');
 
-
-
- // Настройки почты отправителя
- $mail->Host       = 'smtp.yandex.com'; // SMTP сервера вашей почты
- $mail->Username   = 'zheskarkarpov@yandex.ru'; // Логин на почте
- $mail->Password   = 'qljqdbatmwfgfqup'; // Пароль на почте
- $mail->SMTPSecure = 'ssl';
- $mail->Port       = 465;
-
- $mail->setFrom('zheskarkarpov@yandex.ru', 'Заявка с сайта'); // Адрес самой почты и имя отправителя
-
-// $mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('ksenia.karpova.4@mail.ru');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
+// Настройки письма
+$mail->isHTML(true);
 $mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->Body = '' . $name . ' оставил заявку, его телефон ' . $phone . '<br>Почта этого пользователя: ' . $email;
 $mail->AltBody = '';
 
-if(!$mail->send()) {
-    echo 'Error';
+if (!$mail->send()) {
+    echo 'Ошибка при отправке сообщения: ' . $mail->ErrorInfo;
 } else {
-    header('location: thank-you.html');
+    header('Location: thank-you.html');
 }
 ?>
